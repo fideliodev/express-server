748df35326948aef0ce1d0a7611f78e57644a88e
andrek <zblackoez@gmail.com>
2023-02-26 03:01:42 -0400

    templates, e valores

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 //caminhos de pasta
 const path = require('path')
+
+
 //rotas
 //MVC - MODEL VIEW CONTROLLER
 //MODEL = TODO QUE VAI LIGAR COM O DB
 //ViEW = HTML
 //CONTROLLER = GERENCIAR OS DADOS 
 //arquivos estáticos e publicos|| estaticos = arquivo.html
+
 //mostrar o caminho dos arquivos html
 const StaticFolder = path.join(__dirname, 'views')
 //mostrar os arquivos
@@ -15,15 +19,36 @@ const expressStatic = express.static(StaticFolder)
 //app usa o expressStatic
 app.use(expressStatic)
 
+
+
+
+
 //pasta publica/ no caso arquivos/ sem isso o js e o css não funciona
 //olha no html
 const publicFolder = path.join(__dirname, 'public')
 const expressPublic = express.static(publicFolder)
 app.use(expressPublic)
+
+
+
+
+//views
 app.get('/', (request, response) => {
     //renderizar o html
-    response.render('views/index')
+    response.render('index', {
+        //olha o head.js!
+        title: 'inicio'
+    })
 } )
+
+app.get('/post', (request, response) => {
+    //renderizar o html
+    response.render('posts', {
+        title: 'post'
+
+    })
+} )
+
 app.get('/sobre', (request, response) => {
     response.send('Teste rota sobre')
 } )
